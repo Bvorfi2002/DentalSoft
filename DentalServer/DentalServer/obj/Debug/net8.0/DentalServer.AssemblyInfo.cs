8e9462dfcb8a39587265d24eab564ffee43844c7
Brlr2003 <omaralsafarti03@gmail.com>
2024-07-03 10:58:20 +0200

    a

--- DentalServer/DentalServer/obj/Debug/net8.0/DentalServer.AssemblyInfo.cs
+++ DentalServer/DentalServer/obj/Debug/net8.0/DentalServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentalServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2625c792e5161db477649029e1284494353745b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0932b1230a452682591dd44f9582eee67045b407")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentalServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentalServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
