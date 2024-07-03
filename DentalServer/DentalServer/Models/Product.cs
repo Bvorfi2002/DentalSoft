0932b1230a452682591dd44f9582eee67045b407
Brlr2003 <omaralsafarti03@gmail.com>
2024-07-03 10:54:08 +0200

    smth

--- DentalServer/DentalServer/Models/Product.cs
+++ DentalServer/DentalServer/Models/Product.cs
@@ -1,19 +1,13 @@
-﻿using System;
-
-public class Product
+﻿public class Product
 {
-	public string Description { get; set; }
-	public double Price { get; set; }
-	public int ProductId { get; set; }
-	public string ProductName { get; set; }
-	public string Code { get; set; }
-	public string Unit {  get; set; }
-	public Supplier supplier { get; set; }
-	public int Quantity { get; set; }
-	public int StockAlert {  get; set; }
-
-	public Product() { 
-		Quantity = 0;
-	}
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string Code { get; set; }
+    public string Unit { get; set; }
+    public Supplier supplier { get; set; }
+    public int Quantity { get; set; } = 0;
+    public int StockAlert { get; set; }
 
 }
