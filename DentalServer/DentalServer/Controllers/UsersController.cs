1d1686e88db6751c802ce07c9e4911f0ab40cc52
guy1998 <acifliku21@epoka.edu.al>
2024-07-14 14:17:50 +0200

    Finished with the user controller, still to be checked

---
+++ DentalServer/DentalServer/Controllers/UsersController.cs
@@ -0,0 +1,150 @@
+﻿using DentalServer.Models;
+using DentalServer.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DentalServer.Data;
+
+namespace DentalServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
+        {
+            return await _context.Users
+                .Select(user => new UserDto
+                {
+                    UserId = user.UserId,
+                    Name = user.Name,
+                    Email = user.Email,
+                    UserType = user.UserType,
+                    PaymentDataId = user.PaymentDataId,
+                    ClinicId = user.ClinicId
+                })
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetUser(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return new UserDto
+            {
+                UserId = user.UserId,
+                Name = user.Name,
+                Email = user.Email,
+                UserType = user.UserType,
+                PaymentDataId = user.PaymentDataId,
+                ClinicId = user.ClinicId
+            };
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(Guid id, UpdateUserDto userDto)
+        {
+            if (id != userDto.UserId)
+            {
+                return BadRequest();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Name = userDto.Name;
+            user.Email = userDto.Email;
+            user.UserType = userDto.UserType;
+            user.PaymentDataId = userDto.PaymentDataId;
+            user.ClinicId = userDto.ClinicId;
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(CreateUserDto userDto)
+        {
+            var user = new User
+            {
+                UserId = Guid.NewGuid(),
+                Name = userDto.Name,
+                Email = userDto.Email,
+                Password = userDto.Password,
+                UserType = userDto.UserType,
+                PaymentDataId = userDto.PaymentDataId,
+                ClinicId = userDto.ClinicId
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.UserId }, new UserDto
+            {
+                UserId = user.UserId,
+                Name = user.Name,
+                Email = user.Email,
+                UserType = user.UserType,
+                PaymentDataId = user.PaymentDataId,
+                ClinicId = user.ClinicId
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(Guid id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
+    }
+}
